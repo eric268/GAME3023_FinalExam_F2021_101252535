5c80c6e367746c6821706377d89a3fcbe8597e1f
Eric Galway <ericwilliamgalway@gmail.com>
2021-12-16 17:26:48 -0500

    Added message box to describe special events

---
+++ Assets/Calender/Scrips/SpecialEventScripts/SpecialEventDescription.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpecialEventDescription : MonoBehaviour
+{
+    public GameObject specialEventMessgaeBox;
+    public string specialEventMessage = "Special Event: None";
+
+    public void MouseEnterEventIcon()
+    {
+        if (specialEventMessgaeBox)
+        {
+            specialEventMessgaeBox.SetActive(true);
+            specialEventMessgaeBox.GetComponentInChildren<TextMeshProUGUI>().text = specialEventMessage;
+        }
+        else
+        {
+            Debug.Log("Please attach special event message box game object to special event icon");
+        }
+
+    }
+
+    public void MouseExitEventIcon()
+    {
+        specialEventMessgaeBox.SetActive(false);
+    }
+}
